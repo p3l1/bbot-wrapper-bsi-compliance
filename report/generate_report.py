2e4950e297f7eed0a5ff18b33e85574d2d342f5f
Philipp Herzog <philipp@herzo.gs>
2024-04-03 16:38:28 +0200

    Report done

--- report/generate_report.py
+++ report/generate_report.py
@@ -96,7 +96,7 @@ class BSIComplianceReport:
 
     def _template_data(self) -> Union[bool, Path]:
 
-        report_template = LATEX_JINJA_ENV.get_template(str(Path("templates/report.tex.j2")))
+        report_template = LATEX_JINJA_ENV.get_template(str(self.template_path))
         german_date_format = "%d.%m.%Y um %H:%M:%S"
 
         self.scan_id = {e.get("scan") for e in self.data if self.disable_scan_id_checking}
@@ -106,20 +106,38 @@ class BSIComplianceReport:
         else:
             self.logger.warning("Detected multiple bbot scan ids. Report is based on data from multiple scans!")
 
-        self.report_source_events = {(f"{e.get("data").get("host")}:{e.get("data").get("port")}", datetime.fromtimestamp(e.get("timestamp"), UTC).strftime(german_date_format)) for e in self.data}
+        report_source_event_type_filter = [
+            "BSI_COMPLIANCE_RESULT",
+            "VULNERABILITY"
+            "FINDING",
+        ]
+
+        """
+        Create set (to eliminate duplicates) with tuples containing a string host:port and the timestamp of the actual
+        scan. Only BSI_COMPLIANCE_RESULT, FINDING and VULNERABILITY events are taken into account. 
+        """
+        self.report_source_events = {
+            (f"{e.get("data").get("host")}:{e.get("data").get("port")}",
+             datetime.fromtimestamp(e.get("timestamp"), UTC).strftime(german_date_format)) for e in self.data
+            if isinstance(e.get("data"), dict) and e.get("type") in report_source_event_type_filter
+        }
 
         self.tls_compliance_events = [e.get("data") for e in self.data if "bsi_compliance_tls" == e.get("module")]
         self.ssh_compliance_events = [e.get("data") for e in self.data if "bsi_compliance_ssh" == e.get("module")]
         self.ipsec_compliance_events = [e.get("data") for e in self.data if "bsi_compliance_ipsec" == e.get("module")]
+        self.vulnerabilities = [e.get("data") for e in self.data if "VULNERABILITY" == e.get("type")]
+        self.findings = [e.get("data") for e in self.data if "FINDING" == e.get("type")]
 
         renderer_template = report_template.render(scan_ids=self.scan_id,
                                                    tls_compliance_events=self.tls_compliance_events,
                                                    ssh_compliance_events=self.ssh_compliance_events,
                                                    ipsec_compliance_events=self.ipsec_compliance_events,
-                                                   report_source_events=self.report_source_events)
+                                                   report_source_events=self.report_source_events,
+                                                   vulnerabilities=self.vulnerabilities,
+                                                   findings=self.findings)
 
         # Write rendered LaTeX to file
-        latex_report_path = Path("output/report.tex").absolute()
+        latex_report_path = Path("report/output/report.tex").absolute()
         try:
             with open(latex_report_path, 'w') as latex_report:
                 latex_report.write(renderer_template)
@@ -132,7 +150,7 @@ class BSIComplianceReport:
     def _render_pdf(self, templated_latex_path: Path) -> Union[bool, Path]:
 
         if self.use_pdflatex:
-            command = ["pdflatex", f"-output-directory={self.output_path.parent.absolute()}", str(templated_latex_path.absolute())]
+            command = ["pdflatex", f"-output-directory={self.output_path.parent.absolute()}", str(templated_latex_path.resolve())]
             subprocess.run(command)
             return self.output_path
 
@@ -140,8 +158,8 @@ class BSIComplianceReport:
             # Convert LaTeX file to PDF
             pypandoc.convert_file(templated_latex_path, 'latex', outputfile=self.output_path)
 
-            # Delete templated LaTeX file again
-            if self.log_level > logging.DEBUG:
+            # Delete templated LaTeX file again if debug
+            if self.log_level > logging.DEBUG or True:
                 os.unlink(templated_latex_path)
 
             return self.output_path
@@ -161,11 +179,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process BBOT output.ndjson and create PDF report based on LaTeX "
                                                  "Jinja2 template")
     parser.add_argument("-j", "--ndjson", type=str, help="Path to the output.ndjson",
-                        metavar="<PATH>", default=Path("../scans/bsi_compliance/output.ndjson"))
+                        metavar="<PATH>", default=Path("scans/bsi_compliance/output.ndjson"))
     parser.add_argument("-o", "--output", type=str, help="Path where the report should be exported to",
-                        metavar="<PATH>", default=Path("output/report.pdf"))
+                        metavar="<PATH>", default=Path("report/output/report.pdf"))
     parser.add_argument("-t", "--template", type=str, help="Path to the report.tex.j2",
-                        metavar="<PATH>", default=Path("templates/report.tex.j2"))
+                        metavar="<PATH>", default=Path("report/templates/report.tex.j2"))
     parser.add_argument("-d", "--debug", help="Enable debug logging, disable deletion of templated "
                                               "LaTeX file", action='store_true')
     parser.add_argument("--disable-scan-id-checking", help="Prevents generation "
@@ -188,8 +206,8 @@ if __name__ == "__main__":
         except json.JSONDecodeError:
             print("Error decoding JSON:", line)
 
-    args_template_path = Path(args.template).resolve()
-    args_output_path = Path(args.output).resolve()
+    args_template_path = Path(args.template)
+    args_output_path = Path(args.output)
 
     report = BSIComplianceReport(args_template_path,
                                  args_output_path,
